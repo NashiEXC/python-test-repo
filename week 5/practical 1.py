ec958446a10d2ded5061aa38796d9073172acaa5
NashiEXC <miaojunius@gmail.com>
2025-05-20 09:39:50 +0800

    asgagsgasgs

---
+++ week 5/practical 1.py
@@ -0,0 +1,12 @@
+grade = input("enter your grade (A to D): ")
+
+if grade == "a" or "A":
+    print ("GPA= 4.0")
+elif grade == "b" or "B":
+    print("GPA= 3.0")
+elif grade == "c" or "C":
+    print("GPA= 2.0")
+elif grade == "d" or "D":
+    print("GPA= 1.0")
+else:
+    print("fucking dumbass LMAOOOO")
